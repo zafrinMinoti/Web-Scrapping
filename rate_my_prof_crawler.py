50349f59f321986655f5e98961716e15d2446d0c
Zafrin Minoti <zafrin.minoti@gmail.com>
2018-06-02 23:33:49 -0400

    basic, quality, tags implimented. reviews in progress.

--- rate_my_prof_crawler.py
+++ rate_my_prof_crawler.py
@@ -7,17 +7,17 @@ from ast import literal_eval
 root = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid='
 
 def basic_info(soup):
-	prof = dict()
+	basic = dict()
 	script = soup.select('script')[3].text
 	info_dict = re.search(
 		'pageLevelData:\s({.+}),\s*reloadInterval:', 
 		script).group(1)
 	basic_info = literal_eval(info_dict)
 
-	prof['name'] = basic_info['prop7']
-	prof['school'] = basic_info['prop6']
-	prof['school_id'] = basic_info['schoolid']
-	prof['subject'] = basic_info['prop3']
+	basic['name'] = basic_info['prop7']
+	basic['school'] = basic_info['prop6']
+	basic['school_id'] = basic_info['schoolid']
+	basic['subject'] = basic_info['prop3']
 
 	# fname = soup.select('span.pfname')[0].get_text().strip()
 	# mname = soup.select('span.pfname')[1].get_text().strip()
@@ -32,24 +32,66 @@ def basic_info(soup):
 	# print(name)
 
 	school = soup.select('h2.schoolname')[0].get_text().split(',')
-	prof['city'] = school[1].strip()
-	prof['state'] = school[2].strip()
+	basic['city'] = school[1].strip()
+	basic['state'] = school[2].strip()
 
-	return prof
+	return basic
+
+def quality_info(soup):
+	quality = dict()
+	all_qualities = soup.select('div.grade')
+	quality['overall_quality'] = all_qualities[0].get_text().strip()
+	quality['take_again'] = all_qualities[1].get_text().strip()
+	quality['level_of_difficulity'] = all_qualities[2].get_text().strip()
+	# quality['hotness'] = re.search('chilis(.+)\.png', str(all_qualities[3])).group(1)
+	# quality['hotness'] = str(all_qualities[3])
+	# print(type(quality['hotness']))
+	# hotness = soup.find_all('figure', text='chilis')
+	hotness = re.search('chilis/(.+)\.png', str(soup)).group(1)
+	# print(hotness)
+	# print(type(hotness))
+	quality['hotness'] = hotness
+
+	return quality
+
+def tags(soup):
+	raw_tags = soup.select('span.tag-box-choosetags')
+	tags = dict()
+	for tag in raw_tags:
+		tag = tag.get_text().strip()
+		tag_key = tag[:-4].strip().lower()
+		tag_count = tag[-2]
+		tags[tag_key] = tag_count
+	return tags
+
+def student_ratings(soup):
+	review_soup = soup.select('div.table.tr')
 
 def get_prof_info(prof_id):
 	prof = dict()
-	page = requests.get(root+str(prof_id))
-	html = page.text
+	# page = requests.get(root+str(prof_id))
+	# html = page.text
+	fhand = open('tippit.html')
+	html = fhand.read()
 	soup = BeautifulSoup(html, 'html.parser')
-
-	# basic_info = name_and_school(soup)
 	
-	prof = basic_info(soup)
 	prof['id'] = prof_id
+	prof.update(basic_info(soup))
+	prof.update(quality_info(soup))
+	prof.update(tags(soup))
 
-
-
+	rating_count = soup.select(
+		'div.table-toggle')[0].get_text().strip().split()[0]
+	print(rating_count)
+	print()
+	review_soup = soup.select('td.rating')
+	print(len(review_soup))
+	print(type(review_soup))
+	bs = BeautifulSoup(review_soup[0].get_text(), 'html.parser')
+	print()
+	print(type(bs))
+	print(bs)
+	print()
 
 	return prof
 	# return type(basic_info)
